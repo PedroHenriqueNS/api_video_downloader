fc31ea05416d6115e163eb0f83b4c12b80d522a6
PedroHenriqueNS <106723520+PedroHenriqueNS@users.noreply.github.com>
2024-05-23 14:41:45 -0300

    update

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ def get_video():
     
 
 
-# app.run(debug=False, threaded=True)
+app.run(debug=False, threaded=True)
 
 # Production
-http_server = WSGIServer(('', 5000), app)
-http_server.serve_forever()
+# http_server = WSGIServer(('', 5000), app)
+# http_server.serve_forever()
